6fbb07361c3934529e8e84031507cf9f067966c5
fapenia <fapenia@gmail.com>
2014-11-11 17:58:48 -0200

    Version 0.1.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject intception-widgets "0.1.5-SNAPSHOT"
+(defproject intception-widgets "0.1.7-SNAPSHOT"
   :description "Widgets for OM/React"
   :url "https://github.com/orgs/intception/"
 
