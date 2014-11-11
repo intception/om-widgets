fa71875662948dfc25dc7eb366da8b2281416052
fapenia <fapenia@gmail.com>
2014-11-11 17:50:42 -0200

    Uses satisfies instead try catch :D

--- src/intception_widgets/utils.cljs
+++ src/intception_widgets/utils.cljs
@@ -2,19 +2,22 @@
   (:require [om.core :as om :include-macros true]
      [om.dom :as dom :include-macros true]))
 
-
 (defn om-update! [tgt key value]
   (if (= nil key)
     (om/update! tgt value)
-    (try
-      (om/set-state! tgt key value)
-      (catch js/Error _
-        (om/update! tgt key value)))))
-
+    (if (satisfies? om/ISetState tgt)
+      (if (fn? value)
+        (om/update-state! tgt key value)
+        (om/set-state! tgt key value))
+      (if (fn? value)
+          (if-not (= nil key)
+            (om/transact! tgt key value)
+            (om/transact! tgt value))
+          (om/update! tgt key value)))))
 
 (defn om-get [tgt key]
-  (try
+  (if (satisfies? om/IGetState tgt)
     (om/get-state tgt key)
-    (catch js/Error _
       (let [ks (if (sequential? key) key [key])]
-           (get-in tgt ks)))))
+           (get-in tgt ks))))
+
