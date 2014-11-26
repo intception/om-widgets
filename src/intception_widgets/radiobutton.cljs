ab9842709b022a43bdc3c7876e66b4e2f9eb3b2a
Sebastián Moreno <smoreno.uy@gmail.com>
2014-11-26 16:24:47 -0200

    Radiobutton cosmetic changes and add label class as param

--- src/intception_widgets/radiobutton.cljs
+++ src/intception_widgets/radiobutton.cljs
@@ -3,12 +3,13 @@
      [om.dom :as dom :include-macros true]
      [intception-widgets.utils :as utils]))
 
+
 (defn- radio [entity]
  (reify
    om/IRenderState
-   (render-state [this {:keys [label id path checked-value disabled class-name]}]
+   (render-state [this {:keys [label id path checked-value disabled class-name label-class]}]
      (dom/div #js {:className class-name}
-        (dom/label #js {:className "label"}
+        (dom/label #js {:className (or label-class "label")}
           (dom/input #js {:type "radio"
                           :id id
                           :disabled disabled
@@ -16,30 +17,33 @@
                           :onChange (fn [e]
                                       (utils/om-update! entity path checked-value ))} label))))))
 
-(defn  radiobutton [entity path  {:keys [label class-name id checked-value disabled ] :or {checked-value true class-name "om-widgets-radio"}}]
- ;; entry point
+(defn radiobutton [entity path  {:keys [label class-name id checked-value disabled label-class]
+                                 :or {checked-value true class-name "om-widgets-radio"}}]
   (om/build radio entity {:state {:label label
-                                :id id
-                                :disabled disabled
-                                :class-name class-name
-                                :checked-value checked-value
-                                :path path}}))
+                                  :id id
+                                  :disabled disabled
+                                  :class-name class-name
+                                  :label-class label-class
+                                  :checked-value checked-value
+                                  :path path}}))
 
 (defn- group [entity]
   (reify
     om/IRenderState
-    (render-state [this {:keys [class-name id path options disabled]}]
+    (render-state [this {:keys [class-name label-class id path options disabled]}]
       (apply dom/div nil
          (map (fn [[value label]]
-                  (radiobutton entity path {:label label :disabled disabled :checked-value value})) options)))))
-
+                  (radiobutton entity path {:label label
+                                            :disabled disabled
+                                            :label-class label-class
+                                            :checked-value value})) options)))))
 
 (defn radiobutton-group
-  [entity path {:keys [options id read-only disabled class-name] :or {class-name "om-widgets-radiogroup"}}]
- ;; entry point
-  (om/build group entity {:state {
-                               :options options
-                               :class-name class-name
-                               :disabled disabled
-                               :id (or id path)
-                               :path path}}))
+  [entity path {:keys [options id read-only disabled class-name label-class]
+                :or {class-name "om-widgets-radiogroup"}}]
+  (om/build group entity {:state {:options options
+                                  :class-name class-name
+                                  :label-class label-class
+                                  :disabled disabled
+                                  :id (or id path)
+                                  :path path}}))
