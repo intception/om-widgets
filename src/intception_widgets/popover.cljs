5a621176aaa3808718ea8d801a1b75eec0f47e94
Sebastián Moreno <smoreno.uy@gmail.com>
2014-12-10 21:54:49 -0200

    Popover was not compiling

--- src/intception_widgets/popover.cljs
+++ src/intception_widgets/popover.cljs
@@ -4,9 +4,13 @@
 
 (defn- popover-component [_ owner]
  (reify
+   om/IDisplayName
+      (display-name [_] "PopOver")
+
    om/IInitState
    (init-state [this]
      {:visible false})
+
    om/IRenderState
    (render-state [this {:keys [label id disabled class-name visible body]}]
      (dom/div #js {:className "om-widgets-popover-launcher"}
@@ -17,26 +21,29 @@
                   :disabled disabled
                   :onClick #(do
                               (om/set-state! owner :visible (not visible))
-                              false)
-                  } label)
+                              false)}
+                  label)
 
       (when visible
-        (dom/div #js {:className "om-widgets-click-handler" :onClick #(do (om/set-state! owner :visible false)
-                                                            false)}))
+        (dom/div #js {:className "om-widgets-click-handler"
+                      :onClick #(do (om/set-state! owner :visible false)
+                                  false)}))
 
       (when visible
         (apply dom/div #js {:className "om-widgets-popover"}
             (cond
-              (fn? body)[(body (fn[]
-                                  (om/set-state! owner :visible false))]
+              (fn? body)[(body (fn []
+                                  (om/set-state! owner :visible false)))]
               (seq? body) body
               :else [body])))))))
 
-(defn  popover [label body {:keys [class-name id disabled ] :or {class-name "om-widgets-popover-button"}}]
- ;; entry point
+;; ---------------------------------------------------------------------
+;; Public
+
+(defn  popover [label body {:keys [class-name id disabled ]
+                            :or {class-name "om-widgets-popover-button"}}]
   (om/build popover-component nil {:state {:label label
                                           :id (or id label)
                                           :disabled disabled
                                           :class-name class-name
-                                          :body body
-                                          }}))
+                                          :body body}}))
