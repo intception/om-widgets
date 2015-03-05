ac7855faf2aab7030f8834e4336a2f1f2b7a1d37
Alexis Rodríguez <arodrig@fing.edu.uy>
2015-03-05 11:48:08 -0200

    version bump 0.1.24

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.intception/om-widgets "0.1.23"
+(defproject org.clojars.intception/om-widgets "0.1.24"
   :description "Widgets for OM/React"
   :url "https://github.com/orgs/intception/"
 
