f588e875f0552adb76f5cc40f2070274c9dcecd2
fapenia <fapenia@gmail.com>
2015-05-11 10:43:30 -0300

    Add unchecked and checked values Avoid update a invalid cursor

--- src/om_widgets/textinput.cljs
+++ src/om_widgets/textinput.cljs
@@ -121,15 +121,16 @@
 
 (defn- update-target
   [target owner {:keys [cbtimeout typing-timeout input-format prev-value path onChange private-state] :as state} bInternal]
-  (let [prev-value (:prev-value @private-state)
-        dom-node (:dom-node @private-state)
-        value (convert-output input-format  (.-value dom-node))]
-    (when (not= prev-value value)
-      (do
-        (swap! private-state assoc :cbtimeout nil :prev-value value)
-        (utils/om-update! target path value)
-        (when (and onChange (not bInternal))
-          (onChange value))))))
+  (when target
+    (let [prev-value (:prev-value @private-state)
+          dom-node (:dom-node @private-state)
+          value (convert-output input-format (.-value dom-node))]
+      (when (not= prev-value value)
+        (do
+          (swap! private-state assoc :cbtimeout nil :prev-value value)
+          (utils/om-update! target path value)
+          (when (and onChange (not bInternal))
+            (onChange value)))))))
 
 (defn- fire-on-change
   [target owner {:keys [cbtimeout typing-timeout prev-value path  private-state onChange] :as state}]
