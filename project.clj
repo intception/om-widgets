ad86c35b29d15769ded2279ab98b58fcf22b856f
Sebastián Moreno <smoreno.uy@gmail.com>
2015-05-12 12:30:38 -0300

    version update

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.intception/om-widgets "0.1.29"
+(defproject org.clojars.intception/om-widgets "0.1.30"
   :description "Widgets for OM/React"
   :url "https://github.com/orgs/intception/"
 
