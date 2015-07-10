2cb76ca4a7b3c022b930c40641d87bfe316ba7ab
Sebastián Moreno <smoreno.uy@gmail.com>
2015-07-10 13:25:32 -0300

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.intception/om-widgets "0.1.31"
+(defproject org.clojars.intception/om-widgets "0.1.32"
   :description "Widgets for OM/React"
   :url "https://github.com/orgs/intception/"
 
