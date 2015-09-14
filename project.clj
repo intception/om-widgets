ddf7e7872997201781b83b141798f2c35f91bca1
fapenia <fapenia@gmail.com>
2015-09-14 12:30:17 -0300

    Upgrade version to 2.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.intception/om-widgets "0.2.0"
+(defproject org.clojars.intception/om-widgets "0.2.1"
   :description "Widgets for OM/React"
   :url "https://github.com/orgs/intception/"
 
