e6a1b1fe7637c77690fb9e99697a0a3519467de5
Fabian Peña <fapenia@gmail.com>
2015-12-15 09:42:28 -0300

    Popover: Call to preventDefault in onClick event. Update version to 0.2.19

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.intception/om-widgets "0.2.18"
+(defproject org.clojars.intception/om-widgets "0.2.19"
   :description "Widgets for OM/React"
   :url "https://github.com/orgs/intception/"
 
