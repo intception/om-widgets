84692d871315b7798d5365f4bd6b87370917e8ff
Fabian Peña <fapenia@gmail.com>
2016-01-29 12:29:44 -0300

    bump up version to 0.2.30

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.intception/om-widgets "0.2.29"
+(defproject org.clojars.intception/om-widgets "0.2.30"
   :description "Widgets for OM/React"
   :url "https://github.com/orgs/intception/"
 
