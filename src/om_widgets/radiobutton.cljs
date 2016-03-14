40536cf4ac96ae2ce6b4ba6725d10073489a0928
Sebastián Moreno <smoreno.uy@gmail.com>
2016-03-13 22:21:28 -0300

    radiobutton - fix class and move to sablono

--- src/om_widgets/radiobutton.cljs
+++ src/om_widgets/radiobutton.cljs
@@ -1,6 +1,7 @@
 (ns om-widgets.radiobutton
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
+            [sablono.core :refer-macros [html]]
             [om-widgets.utils :as utils]))
 
 
@@ -9,40 +10,40 @@
   (reify
     om/IRenderState
     (render-state [this {:keys [label id path checked-value disabled class-name label-class tabIndex onChange]}]
-      (dom/div #js {:className (str class-name (if (= (utils/om-get entity [path]) checked-value) " active" ""))}
-               (dom/label #js {:className (str (or label-class "")
-                                               (when disabled
-                                                 " text-muted "))}
-                          (dom/input #js {:type "radio"
-                                          :id id
-                                          :disabled disabled
-                                          :tabIndex tabIndex
-                                          :checked (= (utils/om-get entity [path]) checked-value)
-                                          :onChange (fn [e]
-                                                      (when onChange
-                                                        (onChange checked-value))
-                                                      (utils/om-update! (om/get-props owner) path checked-value)
-                                                      (when (utils/atom? (om/get-props owner))
-                                                        (om/refresh! owner)))})
-                          (dom/span nil label))))))
+      (html
+        [:div {:class ["radio" class-name (if (= (utils/om-get entity [path]) checked-value) "active")]}
+         [:label {:class [label-class (when disabled "text-muted")]}
+          [:input {:type "radio"
+                   :id id
+                   :disabled disabled
+                   :tabIndex tabIndex
+                   :checked (= (utils/om-get entity [path]) checked-value)
+                   :onChange (fn [e]
+                               (when onChange
+                                 (onChange checked-value))
+                               (utils/om-update! (om/get-props owner) path checked-value)
+                               (when (utils/atom? (om/get-props owner))
+                                 (om/refresh! owner)))}]
+          label]]))))
 
 (defn- group
   [entity]
   (reify
     om/IRenderState
     (render-state [this {:keys [path options disabled class-name label-class group-class]}]
-      (apply dom/ul #js {:className group-class}
-             (map (fn [option]
-                    (dom/li nil
-                      (om/build radio entity {:state {:label (:label option)
-                                                      :tabIndex (:tabIndex option)
-                                                      :disabled disabled
-                                                      :class-name class-name
-                                                      :label-class label-class
-                                                      :onChange (:onChange option)
-                                                      :checked-value (:checked-value option)
-                                                      :path path}})))
-                  options)))))
+      (html
+        (utils/make-childs
+          [:div {:class group-class}]
+          (map (fn [option]
+                 (om/build radio entity {:state {:label (:label option)
+                                                 :tabIndex (:tabIndex option)
+                                                 :disabled disabled
+                                                 :class-name class-name
+                                                 :label-class label-class
+                                                 :onChange (:onChange option)
+                                                 :checked-value (:checked-value option)
+                                                 :path path}}))
+               options))))))
 
 
 ;; ---------------------------------------------------------------------
