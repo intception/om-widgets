4f732d226f164c76314543ef46d7f24117f86d2d
ichramm <jramirez.uy@gmail.com>
2021-02-01 15:31:25 -0300

    bump version to 0.3.47

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.intception/om-widgets "0.3.46"
+(defproject org.clojars.intception/om-widgets "0.3.47"
   :description "Widgets for OM/React"
   :url "https://github.com/orgs/intception/"
   :license {:name "Eclipse Public License"
