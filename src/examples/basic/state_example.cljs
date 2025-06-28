f5fa9062dad28f19d512012da620496e0fb96bb6
Fabricio Cozzarolo <fabricioc_8@hotmail.com>
2025-06-28 15:13:02 -0300

    Add initial value to example state

--- src/examples/basic/state_example.cljs
+++ src/examples/basic/state_example.cljs
@@ -72,7 +72,8 @@
      :datepicker {:inline #inst "1991-01-25"
                   :input-group-left #inst "1991-01-25"
                   :input-group-right #inst "1991-01-25"
-                  :input-group-close-on-change #inst "1991-01-25"}
+                  :input-group-close-on-change #inst "1991-01-25"
+                  :input-group-date-format #inst "1991-01-25"}
      :sex :male
      :tab {:selected-tab :inbox}
      :form {:name ""
